588005b40b56e313ae3045b6e640ea2452a509f0
Jacob A. Anderson <BH05230@tn.gov>
2022-05-05 11:27:36 -0500

    updated

---
+++ Components/Navbar.js
@@ -0,0 +1,42 @@
+import {View, Text, Button} from 'react-native'
+import {Icon} from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native';
+const Navbar = (props) =>{
+    const navigation = useNavigation()
+
+    function login(){
+        navigation.navigate('Login', {loggedIn:false})
+        props.renderLogin()
+    }
+
+    function logout(){
+        navigation.navigate('Home', {loggedIn: false})
+        props.renderLogin()
+    }
+
+
+    if(props.loggedIn == false){
+        return(
+            <View style={{margin:10, display:'flex', flexDirection:'row', height:'10%', justifyContent:'space-between', alignItems:'center'}}>
+                <Icon onPress={()=> navigation.goBack()}  size={60} type='evilicon' name='arrow-left'/>
+                <Icon onPress={()=> navigation.navigate('Account')} size={60} type='evilicon' name='user'/>
+                <Button  title="Login" onPress={()=> login()}/>
+            </View>
+        )
+    }
+
+
+    else{
+        return (
+            <View style={{margin:10, display:'flex', flexDirection:'row', height:'10%', justifyContent:'space-between', alignItems:'center'}}>
+                <Icon onPress={()=> navigation.goBack()}  size={60} type='evilicon' name='arrow-left'/>
+                <Icon size={60} type='evilicon' name='user'/>
+                <Button  title="Logout" onPress={()=> logout()}/>
+            </View>
+        )
+    }
+
+
+}
+
+export default Navbar;
---
+++ Screens/Account.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+
+
+const Account = () =>{
+    return(
+        <View style={{justifyContent:'center', flex:1, alignItems:'center'}}>
+            <Text>USER ACCOUNT</Text>
+        </View>
+    )
+}
+
+export default Account;
